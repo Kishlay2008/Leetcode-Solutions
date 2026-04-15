607a5fe6a7a4f432d372b8805e5b175d0788e131
kishlay raj <kishlayraj007@outlook.com>
2026-04-16 01:01:29 +0530

    Add files via upload

---
+++ Minimum capacity box(3861)/minimumCapacityBox.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int minimumIndex(int[] capacity, int itemSize) {
+        int min = Integer.MAX_VALUE;
+        int idx = -1;
+        for(int i = 0; i < capacity.length; i++){
+            if(capacity[i] >= itemSize && min > capacity[i]){
+                min = capacity[i];
+                idx = i;
+            }
+        }
+        return idx;
+    }
+} {
+    
+}
