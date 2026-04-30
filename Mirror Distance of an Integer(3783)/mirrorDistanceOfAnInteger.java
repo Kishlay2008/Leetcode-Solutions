57597bd5f11b98810f62a1d3adceb1c902042693
kishlay raj <kishlayraj007@outlook.com>
2026-04-30 15:36:54 +0530

    Add files via upload

---
+++ Mirror Distance of an Integer(3783)/mirrorDistanceOfAnInteger.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int mirror(int n){
+        int rev = 0;
+        while(n != 0){
+            rev = rev*10+n%10;
+            n  /= 10;
+        
+        }
+        return rev;
+    }
+    public int mirrorDistance(int n) {
+        return Math.abs(n-mirror(n));
+    }
+}
